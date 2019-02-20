510512e1412ada679adb8b1a53e7a24961021854
TheJadeDrag0n <47540316+TheJadeDrag0n@users.noreply.github.com>
2019-02-20 15:11:33 +1300

    Added image folder

---
+++ Sign-Up-Ver-1.4.1/Sign Up.py
@@ -0,0 +1,52 @@
+#---------------------------------------------------
+#| Ver 1.1 - added test ticket data                |
+#| Ver 1.2 - adding server functionality to pyhton |
+#---------------------------------------------------
+
+
+from bottle import run, route, view, get, post, request
+from itertools import count
+
+
+
+class Ticket:
+    
+    
+    _ids = count(0)
+    
+    def __init__(self, name, email, date_of_birth,check_in):
+        
+        self.id = next(self._ids)
+        self.name = name
+        self.email = email
+        self.dob = date_of_birth
+        self.check_in = check_in
+
+
+
+
+#test data
+tickets = [
+    Ticket("James Cameron", "jamescameron@my.rnls.school.nz", "21/09/01", False),
+    Ticket("Dan", "dan123@outlook.com", "13/06/87", False),
+    Ticket("Hannah Wilson", "hannah123@gmail.com", "n/a", False),
+    Ticket("Cynthia", "randomperson@gmail.com", "n/a", False)
+    ]
+
+
+
+
+
+#Pages
+
+#index
+@route("/")
+@view("index")
+def index():
+    
+    pass
+
+
+
+
+run(host='0.0.0.0',port = 8080, reloader=True, debug=True)
